cccfddb2e7dbc83931868952192503d30360239d
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-26 20:48:23 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+class Dog
+    attr_reader :name
+
+    #used to save all the dogs that are instantiated
+    @@all = []
+    def initialize(name)
+        @name = name
+        #the instantiated dog will save itself
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        #all is the array of dogs that were instantiated
+        @@all.each do |dog| #goes through each 
+            #print each dog name
+            puts dog.name
+        end
+    end
+
+    def save
+        #self refers to the dog that is instantiated
+        @@all << self
+    end
+end
