7790f6e5590380121e4402eee01f2e11221604e4
21shield <21netaly@gmail.com>
2020-06-28 19:52:25 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+    @@all = []
+    attr_reader :name
+    def initialize (name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.print_all
+        self.all.each do |doggo|
+            puts doggo.name
+        end
+    end
+    def self.clear_all
+        self.all.clear
+    end
+
+end
